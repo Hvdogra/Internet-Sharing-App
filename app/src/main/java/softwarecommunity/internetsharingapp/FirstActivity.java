899e94bd4d948aef368846ef9555fcb89cb4c906
Sachin Bijalwan <2015csb1027@iitrpr.ac.in>
2018-02-04 21:55:10 +0000

    Made Buttons active

--- app/src/main/java/softwarecommunity/internetsharingapp/FirstActivity.java
+++ app/src/main/java/softwarecommunity/internetsharingapp/FirstActivity.java
@@ -1,7 +1,9 @@
 package softwarecommunity.internetsharingapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
+    }
+    public void openSecondActivity(View v) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFourthActivity(View v) {
+        Intent intent = new Intent(this, FourthActivity.class);
+        startActivity(intent);
     }
 }
